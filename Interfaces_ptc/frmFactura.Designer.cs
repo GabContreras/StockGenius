c7aaa33b4c14d2c32cfa92cb1dc3493a5b22ab78
gabContrerasc <138597477+gabContrerasc@users.noreply.github.com>
2023-09-15 21:46:46 -0600

    icono agregado (se ve feo)

--- Interfaces_ptc/frmFactura.Designer.cs
+++ Interfaces_ptc/frmFactura.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFactura));
             this.rpFactura = new Microsoft.Reporting.WinForms.ReportViewer();
             this.SuspendLayout();
             // 
@@ -50,6 +51,7 @@
             this.ClientSize = new System.Drawing.Size(832, 749);
             this.Controls.Add(this.rpFactura);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "frmFactura";
--- Interfaces_ptc/frmmenu.Designer.cs
+++ Interfaces_ptc/frmmenu.Designer.cs
@@ -317,6 +317,7 @@
             this.Controls.Add(this.panelContenedor);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.ImeMode = System.Windows.Forms.ImeMode.Off;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
